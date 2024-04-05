61f71e56463286a889fb64c7682f3cf96ce69703
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-04-05 19:37:16 -0300

    Update ListRender.jsx

--- 3_AvancandoEmReact/avancando/src/components/ListRender.jsx
+++ 3_AvancandoEmReact/avancando/src/components/ListRender.jsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
 
 const ListRender = () => {
-const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
-  
+  const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
+
+  const [users] = useState([
+    { id: 1, name: "Moisés", age: 21 },
+    { id: 72378423, name: "João", age: 28 },
+    { id: 8328823, name: "Pedro", age: 44 },
+  ]);
 
   return (
-  <div>
-    <ul>
+    <div>
+      <ul>
         {list.map((item, i) => (
           <li key={i}>{item}</li>
         ))}
       </ul>
-  </div>
- 
- );
-}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ListRender;
