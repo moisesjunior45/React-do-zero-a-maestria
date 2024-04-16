02d436895e686e4ac60ebd2ec1dab35ae1894f8c
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-04-15 21:46:23 -0300

    If e else em react, props, destructuring e reaproveitamento de componentes

--- 3_AvancandoEmReact/avancando/src/App.js
+++ 3_AvancandoEmReact/avancando/src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 import City from './assets/city.jpg';
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
+import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
+import { useState } from 'react';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  const name = "Joaquim";
+  const [userName] = useState("Maria")
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -18,6 +25,14 @@ function App() {
       </div>
       <ManageData />
       <ListRender />
+      <ConditionalRender />
+      {/* props */}
+      <ShowUserName name={userName} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
+      {/* reaproveitando */}
+      <CarDetails brand="Ford" color="vermelha" km={0} newCar={true}/>
+      <CarDetails brand="Fiat" color="Branco" km={4500} newCar={false} />
     </div>
   );
 }
