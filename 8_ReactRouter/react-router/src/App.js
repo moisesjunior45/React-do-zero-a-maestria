3b8dbe493e08829461bbd05f72c25ed40e094e2c
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-06-25 14:20:48 -0300

    Iniciando os estudos em React Router

--- 8_ReactRouter/react-router/src/App.js
+++ 8_ReactRouter/react-router/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
+// 1 - config react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+
+// components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+      <BrowserRouter>
+      {/* 2 - links com react router */}
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ 8_ReactRouter/react-router/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// 2 - links com react router
+import "./Navbar.css";
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
