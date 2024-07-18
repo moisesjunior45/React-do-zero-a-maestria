af69c5191f9b28ab8caa7550cd56e52851d38609
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-07-17 21:46:40 -0300

    Finalizando módulo

--- 8_ReactRouter/reactrouter/src/App.js
+++ 8_ReactRouter/reactrouter/src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 
 // 1 - config react router, sem links
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// components
+import Navbar from "./components/Navbar";
+import SearchForm from "./components/SearchForm";
 
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
-
-// 2 - adicionando links
-// components
-import Navbar from "./components/Navbar";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar />
+        {/* 9 - search */}
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
@@ -26,6 +30,12 @@ function App() {
           <Route path="/products/:id" element={<Product />} />
           {/* 6 - nested routes */}
           <Route path="/products/:id/info" element={<Info />} />
+          {/* 9 - search */}
+          <Route path="/search" element={<Search />} />
+          {/* 10 - redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
+          {/* 7 - no match route */}
+          <Route path="*" element={<NotFound />} />
 
         </Routes>
       </BrowserRouter>
--- 8_ReactRouter/reactrouter/src/components/Navbar.jsx
+++ 8_ReactRouter/reactrouter/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+      <NavLink
+        to="/"
+        // className={({ isActive }) => (isActive ? "esta-ativo" : "não-ativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   );
 };
