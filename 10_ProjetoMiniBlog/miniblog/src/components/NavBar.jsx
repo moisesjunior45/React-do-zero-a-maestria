627ee7b8d77ce318da5c568751d375f7e874c747
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-08-21 22:36:49 -0300

    Iniciando projeto

---
+++ 10_ProjetoMiniBlog/miniblog/src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+// CSS
+import styles from "./NavBar.module.css";
+
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.navbar}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+
+export default NavBar;
---
+++ 10_ProjetoMiniBlog/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtwzRpA27PuVr95cho4HnXYOsFyrL2Z9Q",
+    authDomain: "mini-blog-8e178.firebaseapp.com",
+    projectId: "mini-blog-8e178",
+    storageBucket: "mini-blog-8e178.appspot.com",
+    messagingSenderId: "858476602069",
+    appId: "1:858476602069:web:fa23b3f7f3f8dfffddb85c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
