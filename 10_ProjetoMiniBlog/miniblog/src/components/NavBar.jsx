b175950fc6b8494c7c59fff948422c2bc7109dac
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-08-24 00:59:41 -0300

    Criando seção de formulário e  deixando menu responsivo

--- 10_ProjetoMiniBlog/miniblog/src/components/NavBar.jsx
+++ 10_ProjetoMiniBlog/miniblog/src/components/NavBar.jsx
@@ -1,36 +1,52 @@
-// CSS
-import styles from "./NavBar.module.css";
-
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import './NavBar.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <nav className={styles.navbar}>
-      <NavLink to="/" className={styles.navbar}>
-        Mini <span>Blog</span>
-      </NavLink>
-      <ul className={styles.links_list}>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Sobre
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+    // Função para fechar o menu
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="navbar">
+            <div className="brand">
+                Mini <span>Blog</span>
+            </div>
+            <ul className={`nav-items ${isOpen ? 'open' : ''}`}>
+                <div className="links-list">
+                    <li>
+                        <NavLink to="/" onClick={closeMenu}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login" onClick={closeMenu}>
+                            Entrar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/register" onClick={closeMenu}>
+                            Cadastrar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" onClick={closeMenu}>
+                            Sobre
+                        </NavLink>
+                    </li>
+                </div>
+            </ul>
+            <div
+                className={`nav-toggle ${isOpen ? 'open' : ''}`}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <div className="bar"></div>
+            </div>
+        </div>
+    );
 };
-
 
 export default NavBar;
