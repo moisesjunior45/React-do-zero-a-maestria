93b946df4eac94fda58358b4b3b7fa3fd15339b7
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-10-07 11:13:36 -0300

    form

--- 5_FormEmReact/form/src/App.js
+++ 5_FormEmReact/form/src/App.js
@@ -4,8 +4,8 @@ import MyForm from './components/MyForm';
 function App() {
   return (
     <div className="App">
-     <h2>Forms</h2>
-     <MyForm user={{name:"Josias", email: "josias@gmail.com", bio: "Sou um advogado", role: "admin" }}/>
+      <h2>Forms</h2>
+      <MyForm user={{ name: "Josias", email: "josias@gmail.com", bio: "Sou um advogado", role: "admin" }} />
     </div>
   );
 }
