3486e62ee30d25675f0b91ad7bd455a513607553
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-12-02 21:39:23 -0300

    Estudando useEffect e useReducer

---
+++ 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+export function HookUseEffect() {
+  // 1 - useEffect, sem dependências
+  useEffect(() => {
+    console.log("Estou sendo executado!");
+  });
+
+  const [number, setNumber] = useState(1);
+
+  const changeSomething = () => {
+    setNumber(number + 1);
+  };
+
+  //   2 - array de deps. vazio
+  useEffect(() => {
+    console.log("Serei executado apenas uma vez!");
+  }, []);
+
+  //   3 - item no array de deps.
+  const [anotherNumber, setAnotherNumber] = useState(0);
+
+  useEffect(() => {
+    if (anotherNumber > 0) {
+      console.log("Sou executador apenas quando o anotherNumber muda!");
+    }
+  }, [anotherNumber]);
+
+  //   4 - cleanup do useEffect
+  useEffect(() => {
+    // const timer = setTimeout(() => {
+    //   console.log("Hello World");
+
+    //   setAnotherNumber(anotherNumber + 1);
+    // }, 2000);
+
+    // return () => clearTimeout(timer);
+  }, [anotherNumber]);
+
+  return (
+    <div>
+      <h2>UseEffect</h2>
+      <p>Number: {number}</p>
+      <button onClick={changeSomething}>Executar</button>
+      <p>Another number: {anotherNumber}</p>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>
+        Mudar anotherNumber
+      </button>
+      <hr />
+    </div>
+  );
+}
--- 11_HOOKS/reacthooks/src/pages/Home.jsx
+++ 11_HOOKS/reacthooks/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { HookUseEffect } from "../components/HookUseEffect";
+import { HookUseReducer } from "../components/HookUseReducer";
 import { HookUseState } from "../components/HookUseState";
 
 export function Home() {
@@ -5,6 +7,8 @@ export function Home() {
     <>
       <h1>Home</h1>
       <HookUseState />
+      <HookUseReducer />
+      <HookUseEffect />
     </>
   );
 }
