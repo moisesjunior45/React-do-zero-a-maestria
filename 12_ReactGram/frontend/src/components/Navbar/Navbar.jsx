15e9a0928d6e864a5dfa3f3e5731e4afadc757f4
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2025-07-14 17:22:15 -0300

    Navbar para usuários autenticados

--- 12_ReactGram/frontend/src/components/Navbar/Navbar.jsx
+++ 12_ReactGram/frontend/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,16 @@ import {
   BsFillCameraFill,
 } from "react-icons/bs";
 
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <>
       <nav id="nav">
@@ -21,17 +30,39 @@ export default function Navbar() {
           <input type="text" placeholder="Pesquisar" />
         </form>
         <ul id="nav-links">
-          <li>
-            <NavLink to="/">
-              <BsHouseDoorFill />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Entrar</NavLink>
-          </li>
-          <li>
-            <NavLink to="/register">Cadastrar</NavLink>
-          </li>
+          {auth ? (
+            <>
+              <li>
+                <NavLink to="/">
+                  <BsHouseDoorFill />
+                </NavLink>
+              </li>
+              {user && (
+                <li>
+                  <NavLink to={`/users/${user._id}`}>
+                    <BsFillCameraFill />
+                  </NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to="/profile">
+                  <BsFillPersonFill />
+                </NavLink>
+              </li>
+              <li>
+                <span>Sair</span>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/login">Entrar</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Cadastrar</NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </>
