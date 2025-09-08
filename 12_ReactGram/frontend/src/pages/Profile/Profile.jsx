f74506950ea3ef7161e3b7ba44abe1cc97167e67
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2025-09-08 13:30:06 -0300

    Exibindo dados e estilos iniciais do usuário

--- 12_ReactGram/frontend/src/pages/Profile/Profile.jsx
+++ 12_ReactGram/frontend/src/pages/Profile/Profile.jsx
@@ -13,12 +13,36 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 // redux
-
+import { getUserDetails } from "../../slices/userSlice";
 
 export default function Profile() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
+
+  // photo
+
+  // load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
     </div>
   );
 }
