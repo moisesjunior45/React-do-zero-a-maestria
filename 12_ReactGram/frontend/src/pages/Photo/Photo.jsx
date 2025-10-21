b8d576242e9af52140e6c39e1c71f8ab7a4a0021
Moisés Júnior <moisesoliveirajr1@gmail.com>
2025-10-21 17:12:02 -0300

    Criando página de foto individual

---
+++ 12_ReactGram/frontend/src/pages/Photo/Photo.jsx
@@ -0,0 +1,18 @@
+import "./Photo.css";
+
+import { uploads } from "../../utils/config";
+
+// components
+import Message from "../../components/Message/Message";
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Redux
+
+export default function Photo() {
+  return <div>Photo</div>;
+}
