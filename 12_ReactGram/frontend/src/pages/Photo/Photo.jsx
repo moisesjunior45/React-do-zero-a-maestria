61f6c554913c1f6738d8a48f679a2e37f9817e28
Moisés Júnior <moisesoliveirajr1@gmail.com>
2025-10-21 17:50:15 -0300

    Exibindo imagem e estilizando página de foto individual

--- 12_ReactGram/frontend/src/pages/Photo/Photo.jsx
+++ 12_ReactGram/frontend/src/pages/Photo/Photo.jsx
@@ -5,6 +5,7 @@ import { uploads } from "../../utils/config";
 // components
 import Message from "../../components/Message/Message";
 import { Link } from "react-router-dom";
+import PhotoItem from "../../components/PhotoItem/PhotoItem";
 
 // Hooks
 import { useState, useEffect } from "react";
@@ -12,7 +13,33 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 // Redux
+import { getPhoto } from "../../slices/photoSlice";
 
 export default function Photo() {
-  return <div>Photo</div>;
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { photo, loading, error, message } = useSelector(
+    (state) => state.photo
+  );
+
+  // Comentários
+
+  // Load photo data
+  useEffect(() => {
+    dispatch(getPhoto(id));
+  }, [dispatch, id]);
+
+  // like e comentário
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="photo">
+      <PhotoItem photo={photo} />
+    </div>
+  );
 }
