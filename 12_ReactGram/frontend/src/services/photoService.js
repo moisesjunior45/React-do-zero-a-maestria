fed0d0a80d6b5e7f363e606b3f5b930dce47d1ae
Moisés Júnior <moisesoliveirajr1@gmail.com>
2025-10-27 18:12:01 -0300

    Service e slice da home

--- 12_ReactGram/frontend/src/services/photoService.js
+++ 12_ReactGram/frontend/src/services/photoService.js
@@ -98,6 +98,20 @@ const comment = async (data, id, token) => {
   }
 };
 
+// Get all photos
+const getPhotos = async () => {
+  const config = requestConfig("GET");
+
+  try {
+    const res = await fetch(`${api}/photos/`, config);
+    const result = await res.json();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
@@ -106,6 +120,7 @@ const photoService = {
   getPhoto,
   like,
   comment,
+  getPhotos,
 };
 
 export default photoService;
